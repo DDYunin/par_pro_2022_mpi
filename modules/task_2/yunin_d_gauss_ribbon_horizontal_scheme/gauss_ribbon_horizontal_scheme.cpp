915b407d44eae739f4b43b84bb3990bdfb51847c
DDYunin <daniilunin2002@yandex.ru>
2022-12-17 09:50:04 +0300

    init struct project

---
+++ modules/task_2/yunin_d_gauss_ribbon_horizontal_scheme/gauss_ribbon_horizontal_scheme.cpp
@@ -0,0 +1,9 @@
+// Copyright 2022 Yunin Daniil
+#include <mpi.h>
+#include <vector>
+#include <string>
+#include <random>
+#include <algorithm>
+#include "../../../modules/task_2/yunin_d_gauss_ribbon_horizontal_scheme/gauss_ribbon_horizontal_scheme.h"
+
+
